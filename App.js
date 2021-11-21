ca14b783170ddf3793e14fc16e98871c96c0b4cf
manbow1206 <tyasunaga@iyell.jp>
2021-11-21 11:27:06 +0900

    [update]add ListItems

--- App.js
+++ App.js
@@ -47,6 +47,19 @@ export default function App() {
       aliquip ex ea commodo consequat."
         author="SampleNews"
       />
+      <ListItem
+        imageUrl="https://picsum.photos/200/300"
+        title="Hello"
+        author="SampleNews"
+      />
+      <ListItem
+        imageUrl="https://picsum.photos/200/300"
+        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+      minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+      aliquip ex ea commodo consequat."
+        author="SampleNews"
+      />
     </View>
   );
 }
