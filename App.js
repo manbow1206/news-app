1abc42fc9e347b037b530ad8e08725a6658feef6
manbow1206 <tyasunaga@iyell.jp>
2021-11-21 12:11:00 +0900

    [add]FlastList

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, View, FlatList, SafeAreaView } from 'react-native';
 import articles from './dummies/articles.json';
 
 import ListItem from './components/ListItem.js';
@@ -9,8 +9,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   itemContainer: {
     height: 100,
@@ -38,16 +36,19 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
-  const items = articles.map((article, index) => {
-    return (
-      <ListItem
-        imageUrl={article.urlToImage}
-        title={article.title}
-        author={article.author}
-        key={index}
-      ></ListItem>
-    );
-  });
-
-  return <View style={styles.container}>{items}</View>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </SafeAreaView>
+  );
 }
--- components/ListItem.js
+++ components/ListItem.js
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
   },
 });
 
-const ListItem = ({ imageUrl, title, author }) => {
+const ListItem = ({imageUrl, title, author}) => {
   return (
     <View style={styles.itemContainer}>
       <View style={styles.leftContainer}>
