12eb82e0cf1c6ce28d60f3802b4c6e9594b22cd2
manbow1206 <tyasunaga@iyell.jp>
2021-11-21 10:11:14 +0900

    [update]change styled position

--- App.js
+++ App.js
@@ -2,22 +2,6 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.itemContainer}>
-        <View style={styles.leftContainer}>
-          <Image
-            style={{ width: 100, height: 100 }}
-            source={{ uri: 'https://picsum.photos/200/300' }}
-          ></Image>
-        </View>
-        <View style={styles.rightContainer} />
-      </View>
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -41,3 +25,19 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+          <Image
+            style={{ width: 100, height: 100 }}
+            source={{ uri: 'https://picsum.photos/200/300' }}
+          ></Image>
+        </View>
+        <View style={styles.rightContainer} />
+      </View>
+    </View>
+  );
+}
