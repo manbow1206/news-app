2b54b0e130a97754a051bc908f8a2fd797aa1628
manbow1206 <tyasunaga@iyell.jp>
2021-11-25 20:59:49 +0900

    [update]App.js

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import AppNavigation from './navigation/AppNavigatior';
-
+import { Provider } from 'react-redux';
+import store from './store';
 
 export default function App() {
   return (
-    <AppNavigation />
+    <Provider store={store}>
+      <AppNavigation />
+    </Provider>
   );
 }
