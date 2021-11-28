f1d490f321d8da60f236546100b9660d2d4843b3
manbow1206 <tyasunaga@iyell.jp>
2021-11-28 17:23:13 +0900

    [add]add icon

--- navigation/AppNavigatior.js
+++ navigation/AppNavigatior.js
@@ -3,6 +3,9 @@ import Constants from 'expo-constants';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome } from '@expo/vector-icons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
 import HomeScreen from '../screens/HomeScreen';
 import ArticleScreen from '../screens/ArticleScreen';
 import ClipScreen from '../screens/ClipScreen';
@@ -27,12 +30,47 @@ const ClipStack = () => {
   );
 };
 
+const scrrenOption = ({ route }) => {
+  tabBarIcon: ({ color, size }) => {
+    let iconName;
+
+    if (route.name === 'Home') {
+      iconName = 'home';
+    } else if (route.name === 'Clip') {
+      iconName = 'bookmark';
+    }
+    return <FontAwesome name={iconName} size={size} color={color} />;
+  };
+};
+
 export default AppNavogator = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeStack} />
-        <Tab.Screen name="Clip" component={ClipStack} />
+      <Tab.Navigator screenOptions={scrrenOption}>
+        <Tab.Screen
+          name="Home"
+          component={HomeStack}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Clip"
+          component={ClipStack}
+          options={{
+            tabBarLabel: 'Clip',
+            tabBarIcon: ({ color, size }) => (
+              <MaterialCommunityIcons
+                name="paperclip"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
