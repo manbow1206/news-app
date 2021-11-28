c99631d605d22a0414ee862ebee259bf8153a940
manbow1206 <tyasunaga@iyell.jp>
2021-11-28 16:48:11 +0900

    [add]createBottomTabNavigator

--- navigation/AppNavigatior.js
+++ navigation/AppNavigatior.js
@@ -2,18 +2,28 @@ import React from 'react';
 import Constants from 'expo-constants';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import ArticleScreen from '../screens/ArticleScreen';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
+      <Stack.Screen name="Article" component={ArticleScreen}></Stack.Screen>
+    </Stack.Navigator>
+  );
+};
 
 export default AppNavogator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen}></Stack.Screen>
-        <Stack.Screen name="Article" component={ArticleScreen}></Stack.Screen>
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeStack} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 };
